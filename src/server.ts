b94f0160edbb41ca67421e6dc377d599203eb8d7
Arthur-Gab <arthurgabriel933@gmail.com>
2023-12-16 23:07:45 -0300

    more complex types and resolver

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,68 @@ import { createSchema, createYoga } from 'graphql-yoga';
 
 const port = process.env.API_PORT || 4000;
 
+/*
+ * Int
+ * Float
+ * String
+ * Boolean
+ * ID
+ */
+
 const typeDefs = /* GraphQL */ `
+	type Product {
+		id: ID!
+		name: String!
+		price: Float!
+	}
+
+	type User {
+		id: ID!
+		name: String!
+		age: Int!
+		salary: Float!
+		isActive: Boolean!
+		techs: [String!]!
+	}
+
 	type Query {
-		grettings: String
+		users: [User!]!
+		products: [Product!]!
 	}
 `;
 
 const resolvers = {
 	Query: {
-		grettings: () => 'Hello from Yoga in a Bun app!',
+		users: () => [
+			{
+				id: 0,
+				name: 'Arthur Gabriel',
+				age: 20,
+				salary: 1200.99,
+				isActive: true,
+				techs: ['TypeScript', 'NextJs', 'GraphQL', 'RESTfull API'],
+			},
+			{
+				id: 1,
+				name: 'Rafaela Clara',
+				age: 16,
+				salary: 700.0,
+				isActive: true,
+				techs: ['Proggraming Logic', 'Portugol'],
+			},
+		],
+		products: () => [
+			{
+				id: 0,
+				name: 'Logitech MX Mini',
+				price: 980.99,
+			},
+			{
+				id: 1,
+				name: 'Logitech LightSpeed G309',
+				price: 320.0,
+			},
+		],
 	},
 };
 
