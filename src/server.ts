ef2c97dadd21c9ae335e6374a1fd9d63f8fd1d93
Arthur-Gab <arthurgabriel933@gmail.com>
2023-12-16 22:25:17 -0300

    undestanding typeDefs and Resolvers

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import { createSchema, createYoga } from 'graphql-yoga';
+
+const port = process.env.API_PORT || 4000;
+
+const typeDefs = /* GraphQL */ `
+	type Query {
+		grettings: String
+	}
+`;
+
+const resolvers = {
+	Query: {
+		grettings: () => 'Hello from Yoga in a Bun app!',
+	},
+};
+
+const yoga = createYoga({
+	schema: createSchema({
+		typeDefs,
+		resolvers,
+	}),
+});
+
+const server = Bun.serve({
+	fetch: yoga,
+	port,
+});
+console.info(
+	`Server is running on ${new URL(
+		yoga.graphqlEndpoint,
+		`http://${server.hostname}:${server.port}`
+	)}`
+);
